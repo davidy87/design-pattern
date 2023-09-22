21d7c7c3010354949bc9ee1ef044c382a6dd4ccb
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2023-09-23 02:30:00 +0900

    Observer pattern implemented

---
+++ src/observer/ObserverTest.java
@@ -0,0 +1,17 @@
+package observer;
+
+public class ObserverTest {
+
+    public static void main(String[] args) {
+        Topic topic = new Topic();
+        Observer a = new TopicSubscriber("a", topic);
+        Observer b = new TopicSubscriber("b", topic);
+        Observer c = new TopicSubscriber("c", topic);
+
+        topic.register(a);
+        topic.register(b);
+        topic.register(c);
+
+        topic.postMessage("This is for test");
+    }
+}
